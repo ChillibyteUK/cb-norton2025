25c86fc8378f69011be7a8100d045d922000e326
ChillibyteUK <dan@chillibyte.co.uk>
2025-12-18 16:53:19 +0000

    single cs

--- single-casestudy.php
+++ single-casestudy.php
@@ -10,7 +10,7 @@ get_header();
 ?>
 <main id="main" class="blog">
 	<section class="breadcrumbs fs-ui mb-4">
-		<div class="container pt-4 pb-5">
+		<div class="container pt-4">
 		<?php
 		if ( function_exists( 'yoast_breadcrumb' ) ) {
 			yoast_breadcrumb( '<div id="breadcrumbs" class="my-2">', '</div>' );
@@ -22,10 +22,10 @@ get_header();
 		<div class="container">
 			<h1><?= esc_html( get_the_title() ); ?></h1>
 			<div class="post_hero">
-				  <?php
-				  if ( has_post_thumbnail() ) {
+				<?php
+				if ( has_post_thumbnail() ) {
 					echo get_the_post_thumbnail( get_the_ID(), 'full', array( 'class' => 'blog_hero__image mb-4' ) );
-				  }
+				}
 				?>
 			</div>
 		</div>
